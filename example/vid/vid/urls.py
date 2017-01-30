dcc042f96fa6c5e13373725e7f298f6d01c1bc1a
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-01-30 17:09:27 +1100

    Add an example application

---
+++ example/vid/vid/urls.py
@@ -0,0 +1,14 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^v/', include('video.urls', namespace='video')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
